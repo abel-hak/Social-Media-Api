a16b164e3a9cb0dd1f250f718497536a82281e72
Abel <hakensoabel@gmail.com>
2025-01-12 17:03:23 +0300

    the last commit

--- social_media_api/users/admin.py
+++ social_media_api/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Profile
+
+# Register the User model to make it available in the admin interface
+admin.site.register(User)
+admin.site.register(Profile)
